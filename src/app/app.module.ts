4e9f3609f939c134384dac79e08f47b51e1dfc7e
Yael Salinas <yael.system10@gmail.com>
2022-02-21 15:37:40 -0600

    Implementacion con Docker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  // {path: 'pages/trabajadores', component: TrabajadoresComponent},
+  // {path: 'pages/bonos', component: BonosComponent},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'pages/compradores', component: CompradoresComponent},
+  // {path: 'pages/productividad', component: ProductividadComponent},
+  // {path: 'pages/inicio', component: InicioComponent},
+  // {path: 'pages/usuarios', component: UsuariosComponent}
+  //{path: 'routes', redirectTo: '/pages', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
